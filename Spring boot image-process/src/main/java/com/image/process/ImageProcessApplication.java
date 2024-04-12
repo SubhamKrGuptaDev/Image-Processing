99d4c41814a3213605cbc90e8420776d21b434ba
subhamkrguptadev <subham7031979@gmail.com>
2024-04-12 08:13:06 +0530

    First Commit (Image Processing APIs with Spring boot)

---
+++ Spring boot image-process/src/main/java/com/image/process/ImageProcessApplication.java
@@ -0,0 +1,13 @@
+package com.image.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessApplication.class, args);
+	}
+
+}
